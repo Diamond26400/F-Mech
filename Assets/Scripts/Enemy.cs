b72037b9f0d0828712f0ab1a850b4688f36179f3
Diamond26400 <obaloluwajames5@gmail.com>
2023-10-23 17:36:55 +0100

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,6 +17,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        enemyRb.AddForce((player.transform.position - transform.position) * speed);
+        enemyRb.AddForce((player.transform.position - transform.position).normalized* speed);
     }
 }
