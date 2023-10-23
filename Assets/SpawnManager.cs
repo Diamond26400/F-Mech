70196bb15b72c8286f95d6af92dbb02a8b1caca3
Diamond26400 <obaloluwajames5@gmail.com>
2023-10-23 19:03:53 +0100

    Update SpawnManager.cs

--- Assets/SpawnManager.cs
+++ Assets/SpawnManager.cs
@@ -9,17 +9,23 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float spawnprosX = Random.Range(-spawnRange, spawnRange);
-        float spawnprosZ = Random.Range(-spawnRange, spawnRange);
 
-        Vector3 randompos = new Vector3(spawnprosX,0, spawnprosZ);
-
-        Instantiate(enemyPreferb,new Vector3(0, 0, 6), enemyPreferb.transform.rotation);
+        Instantiate(enemyPreferb, GenerateSpawnPosition(), enemyPreferb.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnprosX = Random.Range(-spawnRange, spawnRange);
+        float spawnprosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randompos = new Vector3(spawnprosX, 0, spawnprosZ);
+
+        return randompos;
+
     }
 }
