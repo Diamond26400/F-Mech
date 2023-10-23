eb58c8928f48d6084ec2ab62b9b04aede498be00
Diamond26400 <obaloluwajames5@gmail.com>
2023-10-23 12:39:57 +0100

    update script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody PlayerRb;
+    private GameObject FocalPoint;
     public float speed = 12.0f;
     // Start is called before the first frame update
     void Start()
     {
         PlayerRb = GetComponent<Rigidbody>();
+        FocalPoint = GameObject.Find("Rotate-cam");
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float VerticalInput = Input.GetAxis("Vertical");
-       PlayerRb.AddForce(Vector3.forward * VerticalInput * speed );
+       PlayerRb.AddForce(FocalPoint.transform.forward * VerticalInput * speed );
     }
 }
