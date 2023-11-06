2a0145ff4c42c62789e5bbc1584042ea4923e018
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-06 15:53:02 +0100

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,23 +6,23 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 5.0f;
 
-    private Rigidbody enemiesPreferb;
+    private Rigidbody enemyRb;
     private GameObject player;
     
     // Start is called before the first frame update
     void Start()
     {
         //calling Game object and physics
-      
+        enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+
     }
 
     // enemy follow player!
     void Update()
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
-        enemiesPreferb.AddForce(lookDirection * speed);
-        //enemyRb.AddForce(lookDirection * speed);
+        enemyRb.AddForce(lookDirection * speed);
         if (transform.position.y < -10)
         {
             Destroy(gameObject);
