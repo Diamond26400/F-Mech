8fadc1f287496185851ae11ef04678885141a79f
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-08 21:45:43 +0100

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -25,7 +25,7 @@ public class Enemy : MonoBehaviour
         enemyRb.AddForce(lookDirection * speed);
         if (transform.position.y < -10)
         {
-            Destroy(gameObject,5.0f);
+            Destroy(gameObject);
         }
     }
     
