83f34a5acc64f754435146c2383883b41158dc91
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-09 14:20:56 +0100

    Update Bullet.cs

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private float speed = 25.0f;
+    public Transform playerTransform;
+    public Bullet bullet;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,26 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            Shoot();
+        }
         
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the bullet collides with an enemy
+        if (other.CompareTag("Enemy"))
+        {
+            // Destroy the enemy and the bullet
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+    void Shoot()
+    {
+        // Spawn a bullet at the player's position
+        Instantiate(bullet, transform.position, transform.rotation);
     }
 }
